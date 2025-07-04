f57d0ac353b58ab1df2ca18a21947a5ca57bf5cd
Michel Brassard <mb6149@rit.edu>
2025-07-04 22:02:47 +0200

    add comments and reminders

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,12 +1,22 @@
 import * as THREE from 'three'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useFrame, type ThreeElements } from '@react-three/fiber'
 
+/**
+ * A rotating box used as a reference and tutorial for testing out 3D objects
+ * @param props 
+ * @returns Box component
+ */
 export default function Box(props: ThreeElements['mesh']) {
     const meshRef = useRef<THREE.Mesh>(null!)
     const [hovered, setHover] = useState(false)
     const [active, setActive] = useState(false)
     useFrame((_, delta) => (meshRef.current.rotation.x += delta))
+
+    useEffect(() => {
+        document.body.style.cursor = hovered ? 'pointer' : 'auto'
+    }, [hovered])
+
     return (
         <mesh
             {...props}
