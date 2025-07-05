7fa424d726160549dc7d82c20c9067accda77c90
Michel Brassard <mb6149@rit.edu>
2025-07-06 00:13:27 +0200

    fix warning

--- src/components/Sphere.tsx
+++ src/components/Sphere.tsx
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
 import { useEffect, useRef, useState } from 'react'
-import { useFrame, type ThreeElements } from '@react-three/fiber'
+import { type ThreeElements } from '@react-three/fiber'
 import { DragControls } from '@react-three/drei'
 
 /**
